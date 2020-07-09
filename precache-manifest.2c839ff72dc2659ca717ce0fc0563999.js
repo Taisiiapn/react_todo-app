e0cb38d1380efc7ca4e926bfa7c5fa9bbf3e81ee
Taisiia Pomazan <taisiia.pomazan@gmail.com>
2020-07-09 19:41:40 +0300

    Updates

---
+++ precache-manifest.2c839ff72dc2659ca717ce0fc0563999.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d63a23e783690202d2c5bbd4c2ef425d",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "f619d283d4e2fa205c30",
+    "url": "/react_todo-app/static/css/main.4e35ba4a.chunk.css"
+  },
+  {
+    "revision": "2bf3f81002f2b8f8f748",
+    "url": "/react_todo-app/static/js/2.6af7d438.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react_todo-app/static/js/2.6af7d438.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f619d283d4e2fa205c30",
+    "url": "/react_todo-app/static/js/main.2987626f.chunk.js"
+  },
+  {
+    "revision": "c1fdaf2e82fd57d968fe",
+    "url": "/react_todo-app/static/js/runtime-main.72276659.js"
+  }
+]);
